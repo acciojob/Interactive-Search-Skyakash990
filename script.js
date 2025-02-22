4d5a2ead280cd56ef7f7cb01decd5b7982dda250
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-22 10:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.getElementById("searchButton").addEventListener("click", function() {
+    let searchDiv = document.querySelector(".search");
+    let searchInput = document.getElementById("searchInput");
+
+    searchDiv.classList.add("active"); // Add active class
+    searchInput.focus(); 
+});
